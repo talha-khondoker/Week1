413e15dbdddb93a263a27de155f116280fc79dcd
MD MUSHFIQUR TALHA KHONDOKER <161893411+talha-khondoker@users.noreply.github.com>
2024-03-17 15:54:24 +0600

    Add files via upload

---
+++ Day-3/A_Biscuit_Generator.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, m, p;
+    cin>>n>>m>>p;
+    int k=p/n;
+    int l=k*m;
+    cout<<l;
+    return 0;
+}
